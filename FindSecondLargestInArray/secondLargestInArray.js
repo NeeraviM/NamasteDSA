c0219f679b8aab7fe69773faf6e6fedc78ac0ade
neeravim <neeravimca@gmail.com>
2025-12-24 15:23:51 +0530

    comments added for duplicate elements

--- FindSecondLargestInArray/secondLargestInArray.js
+++ FindSecondLargestInArray/secondLargestInArray.js
@@ -1,6 +1,6 @@
 // Function to find the second largest number in an array. 
 // This will work for both positive and negative numbers.
-// It will not work if the array has duplicate largest numbers.
+// It will work even if the array has duplicate largest numbers.
 
 function secondLargest(nums){
 // If the array has less than 2 elements
